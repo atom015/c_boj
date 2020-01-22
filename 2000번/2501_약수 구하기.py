2cde7b42ed4abc3b66592c5a420a24ddf5eb2d09
zeezlelove <zeezlelove@gmail.com>
2020-01-22 15:29:41 +0900

    백준 문제풀이

---
+++ 2000번/2501_약수 구하기.py
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+using namespace std;
+
+int main(){
+	int n,k;
+	vector<int> arr;
+	scanf("%d %d",&n,&k);
+	for (int i = 1; i <= n; i++){
+		if (n % i == 0){
+			arr.push_back(i);
+		}
+	}
+	if (arr.size() < k){
+		printf("%d",0);
+	}else{
+		printf("%d",arr[k-1]);
+	}
+	return 0;
+}
