6bbc119b12fc41e478a50d270a16fe26e94cc090
zeezlelove <zeezlelove@gmail.com>
2020-02-19 08:43:51 +0900

    백준 문제풀이

--- 14000번/14501_퇴사.cpp
+++ 14000번/14501_퇴사.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
 #include<stdio.h>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(void)
 {
+	int n,ret=0,ans=0;
+	int t[20],p[20],d[20] = {0,};
+	scanf("%d",&n);
+	for (int i=1;i<= n;i++){
+		scanf("%d %d",&t[i],&p[i]);
+	}
+	for (int i = 1;i < n+2;i++){
+		for (int j=1;j<i;j++){
+			d[i] = max(d[i],d[j]);
+			if (j+t[j] == i){
+				d[i] = max(d[j]+p[j],d[i]);
+			}
+		}
+		ans = max(d[i],ans);
+	}
+	printf("%d",ans);
 	
     return 0;
 }
